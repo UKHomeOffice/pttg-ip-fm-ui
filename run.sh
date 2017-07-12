071fe6f8cc02c3b6db5c59e8dfa6d5993e17ac49
Stuart Craigen <s.craigen@mac.com>
2017-07-12 09:14:46 +0100

    rn.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-NAME=${NAME:-pttg-ip-fm-ui}
-
-JAR=$(find . -name ${NAME}*.jar|head -1)
-java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djavax.net.ssl.trustStore=/data/truststore.jks -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+npm start
