33cfa3a5a7b65eaea8c769e025dbfdefce019fbd
Stuart Craigen <s.craigen@mac.com>
2018-06-14 16:01:25 +0100

    WIP

--- src/tests/fm.test.js
+++ src/tests/fm.test.js
@@ -1,29 +1,35 @@
 /* global describe it beforeEach inject _ expect */
 
-var getTestIndividual = function (n) {
+const getTestIndividual = (n) => {
   switch (n) {
     case 'Bilbo':
       return {
         forename: 'Bilbo',
         surname: 'Baggins',
-        nino: 'BB123456B'
+        nino: 'BB123456B',
+        dob: '1970-05-13',
+        employers: ['Bag End', 'The Dwarves', 'Rivendell']
       }
     case 'Gandalf':
       return {
         forename: 'Gandalf',
         surname: 'Mithrandir',
-        nino: 'GG123456G'
+        nino: 'GG123456G',
+        dob: '1920-06-01',
+        employers: ['The Dwarves', 'The Fellowship of the Ring']
       }
     case 'Gollum':
       return {
         forename: 'Gollum',
         surname: 'Smeegol',
-        nino: 'GO123456M'
+        nino: 'GO123456M',
+        dob: '1800-01-01',
+        employers: ['The Ring']
       }
   }
 }
 
-var getTestCheck = function (pass, cat, type) {
+const getTestCheck = (pass, cat, type) => {
   return {
     category: cat,
     calculationType: type,
@@ -49,11 +55,20 @@ var getTestCheck = function (pass, cat, type) {
   }
 }
 
-var getTestResponse = function () {
-  return {
+const getSearchIndividuals = (applicant, partner) => {
+  let individuals = []
+  individuals.push(getTestIndividual(applicant))
+  if (partner) {
+    individuals.push(getTestIndividual(partner))
+  }
+  return individuals
+}
+
+const getTestResponse = (applicant, partner) => {
+  let data = {
     data: {
       individuals: [
-        getTestIndividual('Bilbo')
+        getTestIndividual(applicant)
       ],
       categoryChecks: [
         getTestCheck(false, 'B', 'non-salaried'),
@@ -62,6 +77,12 @@ var getTestResponse = function () {
       ]
     }
   }
+
+  if (partner) {
+    data.individuals.push(getTestIndividual(partner))
+  }
+
+  return data
 }
 
 describe('app: hod.proving', () => {
@@ -275,6 +296,21 @@ describe('app: hod.proving', () => {
 
         var test = fm.getResultSummary()
         expect(test.category).toEqual('A')
+      })
+    })
+
+    describe('getCopyPasteSummary', () => {
+      it('should get an appropriate text summary when copy function is used', () => {
+        let testObj = fm.getSearch()
+        testObj.applicationRaisedDate = '2018-05-13'
+        testObj.dependants = 2
+        testObj.individuals = getSearchIndividuals('Bilbo', 'Gollum')
+
+        var res = getTestResponse()
+        fm.setLastAPIresponse(res)
+
+        let txt = fm.getCopyPasteSummary()
+        console.log(txt)
       })
     })
   })
