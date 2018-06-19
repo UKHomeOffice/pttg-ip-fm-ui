5c8241d6182faa77721b8bd62b014cdeb428eb0b
Stuart Craigen <s.craigen@mac.com>
2018-06-19 16:20:30 +0100

    Moved feedback into separate file and component

---
+++ src/app/modules/familymigration/familymigrationFeedback.js
@@ -0,0 +1,107 @@
+/* global angular _ ga */
+
+var fm = angular.module('hod.familymigration')
+
+fm.directive('fmFeedback', ['IOService', function (IOService) {
+  return {
+    restrict: 'E',
+    scope: {
+      passed: '=',
+      callback: '='
+    },
+    templateUrl: 'modules/familymigration/familymigrationFeedback.html',
+    compile: function (element, attrs) {
+      return function ($scope, element, attrs) {
+        $scope.showForm = true
+        $scope.showThanks = false
+        $scope.feedback = {}
+        $scope.yesNoOptions = [{ value: 'yes', label: 'Yes' }, { value: 'no', label: 'No' }]
+
+        $scope.feedback = { whynot: {} }
+
+        var options
+        if ($scope.passed) {
+          options = [
+            { value: 'failed-a-salaried', label: 'Not Passed on Cat A Salaried' },
+            { value: 'failed-b-nonsalaried', label: 'Not Passed on Cat B Non-Salaried' },
+            { value: 'failed-f', label: 'Not Passed on Cat F Self Assessment (1 Year)' },
+            { value: 'failed-g', label: 'Not Passed on Cat G Self Assessment (2 Years)' }
+          ]
+        } else {
+          options = [
+            { value: 'passed-a-salaried', label: 'Passed on Cat A Salaried' },
+            { value: 'passed-b-nonsalaried', label: 'Passed on Cat B Non-Salaried' },
+            { value: 'passed-f', label: 'Passed on Cat F Self Assessment (1 Year)' },
+            { value: 'passed-g', label: 'Passed on Cat G Self Assessment (2 Years)' }
+          ]
+        }
+
+        $scope.conf = {
+          match: {
+            label: 'Did IPS match the paper assessment?',
+            inline: true,
+            onClick: function (opt, scope) {
+              setFeedbackVisibility(opt.value)
+            }
+          },
+          whynot: {
+            id: 'whynot',
+            options: options
+          },
+          matchOther: {
+            classes: {'form-control-1-4': false},
+            required: false
+          }
+        }
+
+        var setFeedbackVisibility = function (v) {
+          $scope.conf.whynot.hidden = true
+          $scope.conf.matchOther.hidden = true
+
+          if (v === 'no') {
+            $scope.conf.whynot.hidden = false
+            $scope.conf.matchOther.hidden = false
+          }
+        }
+
+        setFeedbackVisibility()
+
+        $scope.feedbackSubmit = function (valid) {
+          if (!valid) return
+          var details = angular.copy($scope.feedback)
+          _.each($scope.conf, function (conf, ref) {
+            if (conf.hidden) {
+              delete details[ref]
+            }
+          })
+
+        // #### END FEEDBACK ####
+
+        // var lastCheckDetails = FamilymigrationService.getFamilyDetails()
+        // details.nino = lastCheckDetails.nino
+          // details.nino = _.pluck(res.data.individuals, 'nino').join(',')
+
+          var feedbackDone = function (ok) {
+          // track
+          // ga('set', 'page', $state.href($state.current.name, $stateParams) + '/' + state + '/feedback/' + details.match)
+          // ga('send', 'pageview')
+            $scope.showForm = false
+            $scope.showThanks = true
+            $scope.$applyAsync()
+            console.log('feedbackDone', $scope.showThanks)
+            if (typeof $scope.callback === 'function') {
+              $scope.callback(ok)
+            }
+          }
+
+          IOService.post('feedback', details).then(function (res) {
+            feedbackDone(true)
+          }, function (err) {
+            console.log('ERROR', err)
+            feedbackDone(false)
+          })
+        }
+      }
+    }
+  }
+}])
