ef6c6878cbd7ab130edb71851b1601bff0eedfd5
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2019-09-04 14:55:48 +0100

    EE-20414: redact individuals data before logging

---
+++ src/assets/utils/utils.js
@@ -0,0 +1,19 @@
+const jsonCopy = (src) => {
+    return !src ? src : JSON.parse(JSON.stringify(src));
+}
+
+const redactedResponse = (body) => {
+    const logOutput = jsonCopy(body)
+
+    if (logOutput) {
+        if (logOutput.categoryChecks) {
+            for (const individual of logOutput.categoryChecks) {
+                delete individual.individuals
+            }
+            delete logOutput.individuals;
+        }
+    }
+    return JSON.stringify(logOutput);
+}
+
+module.exports = redactedResponse;
